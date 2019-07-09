3dac0e845ecd511f546983156cf6aa8d3561533b
Nathan Ong <nro5@pitt.edu>
2019-07-09 09:58:55 -0400

    quick change

--- entity/util/nlp.py
+++ entity/util/nlp.py
@@ -10,7 +10,7 @@ from entity.util.config import config
 # nlp = spacy.load('en')
 nlp = spacy.load('en_core_web_sm')
 
-STEMMER_TYPE = config.LEMMATIZER_WORDNET
+STEMMER_TYPE = config.PORTER
 STEMMER_TYPE_DICT = {config.PORTER:PorterStemmer() , config.LANCASTER:LancasterStemmer() , config.SNOWBALL:EnglishStemmer() , config.LEMMATIZER_WORDNET:WordNetLemmatizer()}
 
 STOPWORD_PATH = 'data/stopword/stopword_en.txt'
